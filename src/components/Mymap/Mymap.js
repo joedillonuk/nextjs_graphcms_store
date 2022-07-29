a9fec58c8b65c98acca5c926cb090be0d7ae975b
Joe Dillon <joe.dillon.uk@gmail.com>
2022-07-29 14:27:02 +0100

    Reverted react, react-dom from v18 to v.17.0.2 which resolved error

--- src/components/Mymap/Mymap.js
+++ src/components/Mymap/Mymap.js
@@ -29,13 +29,15 @@ const Mymap = () => {
   }, []);
 
   return (
-          <div>this is Mymap</div>
-    // <MapContainer center={position} zoom={12}scrollWheelZoom={false}>
-    //   <TileLayer
-    // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    //   />
-    // </MapContainer>
+          <div>this is Mymap
+   <MapContainer center={position} zoom={12}scrollWheelZoom={false}>
+     <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+     </MapContainer>
+
+    </div>
   )
 }
 
